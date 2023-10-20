4e1996bf9d32e771bd1f143814ace4605bbe8458
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2023-10-21 01:17:28 +0300

    first babysteps in bot programming!

--- src/telegram_bot/__main__.py
+++ src/telegram_bot/__main__.py
@@ -1,4 +1,18 @@
 import json
+import asyncio
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def begin(message: Message) -> None:
+    await message.answer(f"Добро пожаловать, {hbold(message.from_user.full_name)}!\n Я — Эфирный Курьер. Я помогу вам быстро и удобно составить накладную, отследить заказ или составить жалобу. Для начала авторизуйтесь.")
+
 
 if __name__ == "__main__":
 
